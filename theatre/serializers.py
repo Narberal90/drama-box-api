b621601a964fa643582454ce1bb6f3a432d55919
Yaroslav Havryliuk <mordegear90@gmail.com>
2024-10-11 21:39:56 +0300

    added TheatreHallSerializer

--- theatre/serializers.py
+++ theatre/serializers.py
@@ -1,12 +1,13 @@
 from rest_framework import serializers
 
-from theatre.models import Actor, Genre, Play, TheatreHall
+from theatre.models import Actor, Genre, Play, TheatreHall, Performance
 
 
 class ActorSerializer(serializers.ModelSerializer):
     class Meta:
         model = Actor
         fields = ("id", "first_name", "last_name", "full_name")
+
 
 class GenreSerializer(serializers.ModelSerializer):
     class Meta:
@@ -25,6 +26,7 @@ class PlaySerializer(serializers.ModelSerializer):
             "actors",
             "genres",
         )
+
 
 class TheatreHallSerializer(serializers.ModelSerializer):
     class Meta:
