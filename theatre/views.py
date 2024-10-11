0abfeaebb3367a75ff2b0b1883e57ff965c920d9
Yaroslav Havryliuk <mordegear90@gmail.com>
2024-10-11 19:11:21 +0300

    added initial view rout and serializer for genres

--- theatre/views.py
+++ theatre/views.py
@@ -1,9 +1,15 @@
 from rest_framework import viewsets
 
-from theatre.models import Actor
-from theatre.serializers import ActorSerializer
+from theatre.models import Actor, Genre
+from theatre.serializers import ActorSerializer, GenreSerializer
 
 
 class ActorViewSet(viewsets.ModelViewSet):
     serializer_class = ActorSerializer
     queryset = Actor.objects.all()
+
+
+class GenreViewSet(viewsets.ModelViewSet
+):
+    queryset = Genre.objects.all()
+    serializer_class = GenreSerializer
