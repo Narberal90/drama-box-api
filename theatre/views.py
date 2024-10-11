d135766992ffe0382791a5b35dbaec0a0b66b177
Yaroslav Havryliuk <mordegear90@gmail.com>
2024-10-11 19:05:56 +0300

    added initial view rout and serializer for actors

--- theatre/views.py
+++ theatre/views.py
@@ -1,3 +1,9 @@
-from django.shortcuts import render
+from rest_framework import viewsets
 
-# Create your views here.
+from theatre.models import Actor
+from theatre.serializers import ActorSerializer
+
+
+class ActorViewSet(viewsets.ModelViewSet):
+    serializer_class = ActorSerializer
+    queryset = Actor.objects.all()
