ba37d823abd42b7e8cdc88a21927ec995deb346e
Yaroslav Havryliuk <mordegear90@gmail.com>
2024-10-12 15:13:38 +0300

    implemented additional routs

--- theatre/urls.py
+++ theatre/urls.py
@@ -1,7 +1,8 @@
 from django.urls import path, include
 from rest_framework import routers
 
-from theatre.views import ActorViewSet, GenreViewSet, PlayViewSet, TheatreHallViewSet, PerformanceViewSet
+from theatre.views import ActorViewSet, GenreViewSet, PlayViewSet, TheatreHallViewSet, PerformanceViewSet, \
+    ReservationViewSet
 
 router = routers.DefaultRouter()
 router.register("actors", ActorViewSet)
@@ -9,6 +10,7 @@ router.register("genres", GenreViewSet)
 router.register("plays", PlayViewSet)
 router.register("theatre_hall", TheatreHallViewSet)
 router.register("performances", PerformanceViewSet)
+router.register("reservations", ReservationViewSet)
 
 urlpatterns = [
     path("", include(router.urls)),
