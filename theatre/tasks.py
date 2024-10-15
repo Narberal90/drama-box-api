d1409251484b32409031901a70b690babb50cc8a
Yaroslav Havryliuk <mordegear90@gmail.com>
2024-10-15 15:21:29 +0300

    implemented message sending using celery and redis

---
+++ theatre/tasks.py
@@ -0,0 +1,11 @@
+from celery import shared_task
+from django.core.mail import send_mail
+from django.conf import settings
+
+@shared_task
+def send_reservation_confirmation_email(reservation_id, user_email):
+    subject = "Reservation Confirmation"
+    message = f"Your reservation has been confirmed. Reservation ID: {reservation_id}"
+    recipient_list = [user_email]
+
+    send_mail(subject, message, settings.EMAIL_HOST_USER, recipient_list)
